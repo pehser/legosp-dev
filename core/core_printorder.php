af86ac91b30aa3ff767d5ac487918de11c0b34f2
empehser/em <empehser@gmail.com/em>
2013-06-04 13:19:13 +0300

    commit

--- core/core_printorder.php
+++ core/core_printorder.php
@@ -1,29 +1,8 @@
 <?php
-       session_start();
+       
        if (isset($_SESSION["order_id"]))
        {
-        include("../cfg/general.inc.php");
-	include("../cfg/connect.inc.php");
-	include("../cfg/appearence.inc.php");
-	include("../includes/database/mysql.php");
-	include("../cfg/company.inc.php");
-	include("../cfg/tables.inc.php");
-	include("../cfg/product.inc.php");
-	include("../cfg/shipping.inc.php");
-	include("../cfg/functions.php");
-	include_once ("./cfg/language_list.php");
-
-	//current language session variable
-if (!isset($_SESSION["current_language"]) || $_SESSION["current_language"] < 0 || $_SESSION["current_language"] > count($lang_list)) $_SESSION["current_language"] = 0; //set default language
-//include a language file
-if (isset($lang_list[$_SESSION["current_language"]]) && file_exists(ROOT_DIR . "/languages/" . $lang_list[$_SESSION["current_language"]]->filename)) include (ROOT_DIR . "/languages/" . $lang_list[$_SESSION["current_language"]]->filename); //include current language file
-else {
-    die("<font color=red><b>ERROR: Couldn't find language file!</b></font>");
-}
-
-	db_connect(DB_HOST,DB_USER,DB_PASS) or die (db_error());
-	db_select_db(DB_NAME) or die (db_error());
-        currency(); 
+        include("../cfg/ajax_connect.inc.php");
         
 		$q = db_query("SELECT orderID, cust_firstname, cust_lastname, cust_email, cust_city, cust_address, cust_phone, comment, manager FROM ".ORDERS_TABLE." WHERE orderID=".(int)$_SESSION["order_id"]) or die (db_error());
 		$result = db_fetch_row($q);
